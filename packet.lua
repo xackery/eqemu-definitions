0c9e4c4becb00428a54d220a16774e299271ded9
demoncia <demoncia>
2022-12-27 11:36:40 +0000

    Cleaned up packet

--- packet.lua
+++ packet.lua
@@ -4,38 +4,49 @@
 ---@class Packet
 Packet = {}
 
+---@returns number
 function Packet:GetOpcode() end
 
 ---@returns number
 function Packet:GetRawOpcode() end
 
+---@returns number
 function Packet:GetSize() end
 
+---@returns number
 function Packet:GetWritePosition() end
 
 ---@param offset number # TODO: definition of parameter
+---@returns number
 function Packet:ReadDouble(offset) end
 
 ---@param offset number # TODO: definition of parameter
 ---@param string_length number # TODO: definition of parameter
+---@returns string
 function Packet:ReadFixedLengthString(offset, string_length) end
 
 ---@param offset number # TODO: definition of parameter
+---@returns number
 function Packet:ReadFloat(offset) end
 
 ---@param offset number # TODO: definition of parameter (and type this)
+---@returns number
 function Packet:ReadInt16(offset) end
 
 ---@param offset number # TODO: definition of parameter (and type this)
+---@returns number
 function Packet:ReadInt32(offset) end
 
 ---@param offset number # TODO: definition of parameter
+---@returns number
 function Packet:ReadInt64(offset) end
 
 ---@param offset number # TODO: definition of parameter (and type this)
+---@returns number
 function Packet:ReadInt8(offset) end
 
 ---@param offset number # TODO: definition of parameter
+---@returns number
 function Packet:ReadString(offset) end
 
 ---@param op number # TODO: definition of parameter
