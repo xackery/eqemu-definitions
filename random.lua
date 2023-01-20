90b6f56a24a31c492578fcf9f644fa54acf70af7
Xackery Xtal <xackery@gmail.com>
2023-01-19 22:27:25 -0800

    Changed random scope

--- random.lua
+++ random.lua
@@ -5,20 +5,20 @@ Random = {}
 
 ---@param low number # min value to randomize range from
 ---@param high number # max value to randomize range from 
-function Random:Int(low, high) end
+function Random.Int(low, high) end
 
 ---@param low number # min value to randomize range from
 ---@param high number # max value to randomize range from 
-function Random:Real(low, high) end
+function Random.Real(low, high) end
 
 ---roll a dice between 1 to value
 ---@param value number # max value to randomize range from
-function Random:Roll(value) end
+function Random.Roll(value) end
 
 ---roll a dice between 1 to value
 ---@param value number # max value to randomize range from
-function Random:RollReal(value) end
+function Random.RollReal(value) end
 
 ---roll a dice between 0 to value
 ---@param value number # max value to randomize range from
-function Random:Roll0(value) end
+function Random.Roll0(value) end
