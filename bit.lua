4e5ccb98bbb974909080f41941c683e147b30ddd
EverQuestParty <EverQuestParty>
2023-01-20 06:44:43 +0000

    Added bit

---
+++ bit.lua
@@ -0,0 +1,63 @@
+---Bit operations
+---@class bit
+bit = {}
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.tobit(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.bnot(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.band(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.bor(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.bxor(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.lshift(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.rshift(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.arshift(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.rol(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.ror(value, shift) end
+
+---@param value number # value base
+---@param shift number # value to shift
+---@return number result # result of shift
+function bit.bswap(value, shift) end
+
+---@param value number # value to convert to hex
+---@param upper number # if set to 8, lowercase hex values
+---@return string result # hex string result
+function bit.tohex(value, upper) end
--- random.lua
+++ random.lua
@@ -1,24 +1,28 @@
-
 ---Contains randomized number generators
 ---@class Random
 Random = {}
 
 ---@param low number # min value to randomize range from
----@param high number # max value to randomize range from 
+---@param high number # max value to randomize range from
+---@return number result # result of roll
 function Random.Int(low, high) end
 
 ---@param low number # min value to randomize range from
 ---@param high number # max value to randomize range from 
+---@return number result # result of roll
 function Random.Real(low, high) end
 
 ---roll a dice between 1 to value
 ---@param value number # max value to randomize range from
+---@return number result # result of roll
 function Random.Roll(value) end
 
 ---roll a dice between 1 to value
 ---@param value number # max value to randomize range from
+---@return number result # result of roll
 function Random.RollReal(value) end
 
 ---roll a dice between 0 to value
 ---@param value number # max value to randomize range from
+---@return number result # result of roll
 function Random.Roll0(value) end
