45d909920fe4d8782ee7389db2841664d66bd2ef
Xackery <xackery@gmail.com>
2024-04-11 04:18:41 -0700

    Fix language

--- const/language.lua
+++ const/language.lua
@@ -1,31 +1,32 @@
 ---@meta
 
----@alias Languages 
----| 0 # Common Tongue
----| 1 # Barbarian
----| 2 # Erudian
----| 3 # Elvish
----| 4 # Dark Elvish
----| 5 # Dwarvish
----| 6 # Troll
----| 7 # Ogre
----| 8 # Gnomish
----| 9 # Halfling
----| 10 # Thieves Cant
----| 11 # Old Erudian
----| 12 # Elder Elvish
----| 13 # Froglok
----| 14 # Goblin
----| 15 # Gnoll
----| 16 # Combine Tongue
----| 17 # Elder Teir'Dal
----| 18 # Lizardman
----| 19 # Orcish
----| 20 # Faerie
----| 21 # Dragon
----| 22 # Elder Dragon
----| 23 # Dark Speech
----| 24 # Vah Shir
----| 25 # Alaran
----| 26 # Hadal
----| 27 # Unknown
+---@class Language
+---@field CommonTongue number -- 0
+---@field Barbarian number -- 1
+---@field Erudian number -- 2
+---@field Elvish number -- 3
+---@field DarkElvish number -- 4
+---@field Dwarvish number -- 5
+---@field Troll number -- 6
+---@field Ogre number -- 7
+---@field Gnomish number -- 8
+---@field Halfling number -- 9
+---@field ThievesCant number -- 10
+---@field OldErudian number -- 11
+---@field ElderElvish number -- 12
+---@field Froglok number -- 13
+---@field Goblin number -- 14
+---@field Gnoll number -- 15
+---@field CombineTongue number -- 16
+---@field ElderTeirDal number -- 17
+---@field Lizardman number -- 18
+---@field Orcish number -- 19
+---@field Faerie number -- 20
+---@field Dragon number -- 21
+---@field ElderDragon number -- 22
+---@field DarkSpeech number -- 23
+---@field VahShir number -- 24
+---@field Alaran number -- 25
+---@field Hadal number -- 26
+---@field Unknown number -- 27
+Language = {}
